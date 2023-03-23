dc5a4df1a224af11001a60d7f446cffd7ae7b70e
maomao <rxm603@case.edu>
2023-03-22 22:17:04 -0400

    plot_CLIP_features_cifar100

--- plot_features_from_Resnet_in_2D.py
+++ plot_features_from_Resnet_in_2D.py
@@ -4,7 +4,10 @@ import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from sklearn.manifold import TSNE
-
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import clip
+import torch.nn.functional as F
 seed = 10
 random.seed(seed)
 torch.manual_seed(seed)
@@ -25,18 +28,55 @@ import csv
 import numpy as np
 
 # Load CSV file
-csv_data = np.loadtxt('C:\\Users\\maoda\\OneDrive\\Desktop\\OSA\\LfOSA\\A_features_epoch99_query_9.csv', delimiter=',')
+# csv_data = np.loadtxt('C:\\Users\\maoda\\OneDrive\\Desktop\\OSA\\LfOSA\\A_features_epoch99_query_9.csv', delimiter=',')
+#
+# # Convert to NumPy array
+# data_array = np.array(csv_data)
+#
+# labels = np.loadtxt('C:\\Users\\maoda\\OneDrive\\Desktop\\OSA\\LfOSA\\A_labels_epoch99_query_9.csv', delimiter=',')
 
-# Convert to NumPy array
-data_array = np.array(csv_data)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
 
-labels = np.loadtxt('C:\\Users\\maoda\\OneDrive\\Desktop\\OSA\\LfOSA\\A_labels_epoch99_query_9.csv', delimiter=',')
+crop = transforms.RandomCrop(32, padding=4)
+normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 
-# Convert to NumPy array
-labels = np.array(labels)
+preprocess_rand = transforms.Compose([crop, transforms.RandomHorizontalFlip(), preprocess])
 
+train_data = datasets.CIFAR100('./data/', train=True, download=True, transform=preprocess_rand)
+record = [[] for _ in range(100)]
+
+batch_size = 256
+print('Data Loader')
+data_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=batch_size, shuffle=False)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+for batch_idx, (index, (data, labels)) in enumerate(data_loader):
+    data = data.to(device)
+    labels = labels.to(device)
+    with torch.no_grad():
+        extracted_feature = model.encode_image(data)
+    for i in range(extracted_feature.shape[0]):
+        record[labels[i]].append({'feature': extracted_feature[i].detach().cpu(), 'index': index[i]})
+
+
+total_len = sum([len(a) for a in record])
+origin_trans = torch.zeros(total_len, record[0][0]['feature'].shape[0])
+origin_label = torch.zeros(total_len).long()
+index_rec = np.zeros(total_len, dtype=int)
+cnt, lb = 0, 0
+for item in record:
+    for i in item:
+        # if i['index'] not in sel_noisy:
+        origin_trans[cnt] = i['feature']
+        origin_label[cnt] = lb
+        index_rec[cnt] = i['index']
+        cnt += 1
+        # print(cnt)
+    lb += 1
+labels = np.array(origin_label)
 # Print shape of array
-tsne = TSNE(n_components=2).fit_transform(csv_data)
+tsne = TSNE(n_components=2).fit_transform(origin_trans)
 print(tsne)
 
 
