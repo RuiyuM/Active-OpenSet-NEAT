8d0c08a591b77daf89d4166018b8d62c19b53826
maomao <rxm603@case.edu>
2023-03-23 20:43:04 -0400

    modified deprecated formate

--- center_loss.py
+++ center_loss.py
@@ -31,7 +31,8 @@ class CenterLoss(nn.Module):
         batch_size = x.size(0)
         distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
                   torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
-        distmat.addmm_(1, -2, x, self.centers.t())
+        # distmat.addmm_(1, -2, x, self.centers.t())
+        distmat.addmm(x, self.centers.t(), beta=1, alpha=-2)
 
         classes = torch.arange(self.num_classes).long()
         if self.use_gpu: classes = classes.cuda()
