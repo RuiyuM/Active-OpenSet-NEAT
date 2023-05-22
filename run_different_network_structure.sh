bb095e54d15d45e0747b041127fad0b5f487f9ec
maomao <rxm603@case.edu>
2023-05-22 14:19:36 -0400

    update

--- run_different_network_structure.sh
+++ run_different_network_structure.sh
@@ -14,14 +14,14 @@ do
         do
             for i in 10
             do
-#                CUDA_VISIBLE_DEVICES=0 python NEAT_main.py --gpu 0 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 20 --query-batch $j --seed 1 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar100 &
-#                CUDA_VISIBLE_DEVICES=1 python NEAT_main.py --gpu 1 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 20 --query-batch $j --seed 2 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar100 &
-#                CUDA_VISIBLE_DEVICES=2 python NEAT_main.py --gpu 2 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 20 --query-batch $j --seed 3 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar100 &
-
-                CUDA_VISIBLE_DEVICES=3 python NEAT_main.py --gpu 3 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 1 --known-class 2 --query-batch $j --seed 1 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar10 &
-                CUDA_VISIBLE_DEVICES=0 python NEAT_main.py --gpu 0 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 1 --known-class 2 --query-batch $j --seed 2 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar10 &
-                CUDA_VISIBLE_DEVICES=1 python NEAT_main.py --gpu 1 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 1 --known-class 2 --query-batch $j --seed 3 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar10 &
+                CUDA_VISIBLE_DEVICES=3 python NEAT_main.py --gpu 3 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 20 --query-batch $j --seed 1 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar100 &
+                CUDA_VISIBLE_DEVICES=2 python NEAT_main.py --gpu 2 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 20 --query-batch $j --seed 2 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar100 &
+                CUDA_VISIBLE_DEVICES=2 python NEAT_main.py --gpu 2 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 20 --query-batch $j --seed 3 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar100 &
                 wait
+#                CUDA_VISIBLE_DEVICES=3 python NEAT_main.py --gpu 3 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 1 --known-class 2 --query-batch $j --seed 1 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar10 &
+#                CUDA_VISIBLE_DEVICES=0 python NEAT_main.py --gpu 0 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 1 --known-class 2 --query-batch $j --seed 2 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar10 &
+#                CUDA_VISIBLE_DEVICES=1 python NEAT_main.py --gpu 1 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 1 --known-class 2 --query-batch $j --seed 3 --model $structure --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset cifar10 &
+
             done
         done
     done
