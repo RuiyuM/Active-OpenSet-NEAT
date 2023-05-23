3b32a57d31902e122e85d56ee26970c2e8e520df
RuiyuM <rxm603@case.edu>
2023-05-22 22:50:42 -0500

    updated the merge version

--- run_different_network_structure.sh
+++ run_different_network_structure.sh
@@ -14,9 +14,9 @@ do
         do
             for i in 10
             do
-                CUDA_VISIBLE_DEVICES=0 python AL_center_temperature.py --gpu 0 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 40 --query-batch $j --seed 1 --model resnet18 --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset Tiny-Imagenet &
-			          CUDA_VISIBLE_DEVICES=0 python AL_center_temperature.py --gpu 0 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 40 --query-batch $j --seed 2 --model resnet18 --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset Tiny-Imagenet &
-			          CUDA_VISIBLE_DEVICES=0 python AL_center_temperature.py --gpu 0 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 40 --query-batch $j --seed 3 --model resnet18 --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset Tiny-Imagenet &
+                CUDA_VISIBLE_DEVICES=0 python NEAT_main.py --gpu 0 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 40 --query-batch $j --seed 1 --model resnet18 --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset Tiny-Imagenet &
+			          CUDA_VISIBLE_DEVICES=0 python NEAT_main.py --gpu 0 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 40 --query-batch $j --seed 2 --model resnet18 --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset Tiny-Imagenet &
+			          CUDA_VISIBLE_DEVICES=0 python NEAT_main.py --gpu 0 --k $i --save-dir log_AL/ --weight-cent 0 --query-strategy $method --init-percent 8 --known-class 40 --query-batch $j --seed 3 --model resnet18 --known-T 0.5 --unknown-T 0.5 --modelB-T 1 --dataset Tiny-Imagenet &
                 wait
             done
         done
